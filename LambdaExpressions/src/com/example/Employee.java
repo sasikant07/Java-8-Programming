5d05e03e51379fa0ea8e5ef387cb5ac84f67f08c
SASIKANTBISWAL <biswalsashikant@gmail.com>
2020-06-04 13:32:10 +0530

    Implemented Java 8 feature Lambda expressions.

---
+++ LambdaExpressions/src/com/example/Employee.java
@@ -0,0 +1,40 @@
+package com.example;
+
+public class Employee {
+	
+	int id;
+	String name;
+	int age;
+	
+	public Employee(int id, String name, int age) {
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	@Override
+	public String toString() {
+		return "Employee [id=" + id + ", name=" + name + ", age=" + age + "]";
+	}
+	
+	
+}
